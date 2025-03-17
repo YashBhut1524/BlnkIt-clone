1dee556748c882d0a34d51b51f46426fb33cf83e
Yash <yashxerox15@gmail.com>
2025-03-17 22:06:14 +0530

    remove last changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,4 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    historyApiFallback: true, // Ensures client-side routing works on refresh
-  },
 });
